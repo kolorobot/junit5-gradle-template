efc29cd8be97eb74f26786fcae467017f7972217
kolorobot <rafal.borowiec@gmail.com>
2018-02-20 01:54:49 +0100

    More examples

--- src/test/java/pl/codeleak/samples/junit5/s07/RestAssuredExtension.java
+++ src/test/java/pl/codeleak/samples/junit5/s07/RestAssuredExtension.java
@@ -1,13 +1,27 @@
 package pl.codeleak.samples.junit5.s07;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.*;
 
-public class RestAssuredExtension implements BeforeAllCallback {
+public class RestAssuredExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
 
     @Override
     public void beforeAll(ExtensionContext context) {
         RestAssured.baseURI = "http://qalabs.pl";
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) {
+
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) {
+
     }
 }
